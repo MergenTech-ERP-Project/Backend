be0db17f261266e54b693ff75b92fe94491c1e37
ceyhunozkann <79968507+ceyhunozkann@users.noreply.github.com>
2022-02-10 16:18:57 +0300

    FindByName was developed

--- src/main/java/com/mergen/vtys/vtysdatabaseap/Controller/UserController.java
+++ src/main/java/com/mergen/vtys/vtysdatabaseap/Controller/UserController.java
@@ -27,8 +27,13 @@ public class UserController {
         return userRepository.findById(id);
     }
 
-    @RequestMapping(value ="/user/users/{name}/{password}",method = RequestMethod.GET)
-    @ResponseBody
+    @GetMapping("/{name}")
+    public Optional<User> getUsersCheck(
+            @PathVariable("name") String name){
+        return  userRepository.findByName(name) ;
+    }
+
+    @GetMapping(value ="/{name}/{password}")
     public Optional<User> getUsersCheck(
             @PathVariable("name") String name,@PathVariable("password") String password) {
         return  userRepository.findByNameAndPassword(name,password) ;
