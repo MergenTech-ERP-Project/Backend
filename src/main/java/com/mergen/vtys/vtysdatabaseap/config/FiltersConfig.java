2478ed9847d772cd453e1a1c621d3438fc2e6594
ceyhunozkann <79968507+ceyhunozkann@users.noreply.github.com>
2022-03-03 13:59:33 +0300

    Experiment Filter

--- src/main/java/com/mergen/vtys/vtysdatabaseap/config/FiltersConfig.java
+++ src/main/java/com/mergen/vtys/vtysdatabaseap/config/FiltersConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.mergen.vtys.vtysdatabaseap.config;
 
 import com.mergen.vtys.vtysdatabaseap.Filters.RequestResponseLoggers;
@@ -15,16 +16,10 @@ public class FiltersConfig {
         registrationBean.setFilter(requestResponseLoggers);
 
         registrationBean.addUrlPatterns(  // Yazılan path'lere göre logging yapılmasını sağlar.
-                "/user/users", "/user/users/*", "/user/*", "/user/*/*", "/user/post", "/user/put/*","/user/delete/*",
-                "/company/companies", "/company/companies/*", "/company/post", "/company/put/*", "/company/delete/*",
-                "/branch/branches", "/branch/branches/*", "/branch/post", "/branch/put/*", "/branch/delete/*",
-                "/activityevaluation/evaluations", "/activityevaluation/evaluations/*", "/activityevaluation/post", "/activityevaluation/put/*", "/activityevaluation/delete/*",
-                "/activity/activities", "/activity/activities/*", "/activity/post", "/activity/put/*", "/activity/delete/*",
-                "/activetouser/activetousers", "/activetouser/activetousers/*","/activetouser/user/*","/activetouser/activity/enrolled/*","/activetouser/post","/activetouser/put/*","/activetouser/delete/*,",
-                "/userdetail/userdetails", "/userdetail/userdetails/*", "/userdetail/post", "/userdetail/put/*", "/userdetail/delete/*"
+
         );
         return registrationBean;
     }
 }
 
-    
+    */
