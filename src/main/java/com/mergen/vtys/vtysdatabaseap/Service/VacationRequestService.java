85204c8bb6ebf6fbf497cbbb249c1a3de1ef74d6
ceyhunozkann <79968507+ceyhunozkann@users.noreply.github.com>
2022-03-31 18:06:34 +0300

    VacationAccrualDto + VacationRequestDto Added

--- src/main/java/com/mergen/vtys/vtysdatabaseap/Service/VacationRequestService.java
+++ src/main/java/com/mergen/vtys/vtysdatabaseap/Service/VacationRequestService.java
@@ -1,5 +1,6 @@
 package com.mergen.vtys.vtysdatabaseap.Service;
 
+import com.mergen.vtys.vtysdatabaseap.Dto.VacationRequestDto;
 import com.mergen.vtys.vtysdatabaseap.Model.VacationRequest;
 import org.springframework.data.repository.query.Param;
 import org.springframework.expression.spel.ast.OpAnd;
@@ -8,9 +9,9 @@ import java.text.ParseException;
 import java.util.List;
 import java.util.Optional;
 
-public interface VacationRequestService extends HelperService<VacationRequest>{
-    List<VacationRequest> getVacationRequestList();
-    Optional<VacationRequest> getVacationRequestById(Long id);
+public interface VacationRequestService extends HelperService<VacationRequestDto>{
+    List<VacationRequestDto> getVacationRequestList();
+    VacationRequestDto getVacationRequestById(Long id);
     String EarnedVacationDays(Long id) throws ParseException;
     String GetDate_of_Start(@Param("id") Long id);
 }
